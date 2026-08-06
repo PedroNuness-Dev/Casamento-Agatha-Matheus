1ad7dfb20e84d5ec070ec93c5c8dbe7ff7ad82f8
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-08-05 23:03:53 -0300

    feat: adiciona os novos componentes no app-root

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderHeroComponent } from './components/header-hero-component/header-hero-component';
+import { HistoryComponent } from './components/history-component/history-component';
+import { ConviteComponent } from './components/convite-component/convite-component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderHeroComponent],
+  imports: [HeaderHeroComponent, HistoryComponent,ConviteComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
