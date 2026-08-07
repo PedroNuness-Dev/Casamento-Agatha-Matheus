81b329a0d4dfff9f7936d2010dc8c633d578dfa2
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-08-06 21:39:47 -0300

    feat: adiciona nova section no componente de convite, colocando um contador para o dia do casamento

--- src/app/components/convite-component/convite-component.ts
+++ src/app/components/convite-component/convite-component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-convite-component',
@@ -6,4 +6,48 @@ import { Component } from '@angular/core';
   templateUrl: './convite-component.html',
   styleUrl: './convite-component.scss',
 })
-export class ConviteComponent {}
+export class ConviteComponent implements OnInit, OnDestroy {
+
+  dias = signal(0);
+  horas = signal(0);
+  minutos = signal(0);
+  segundos = signal(0);
+
+  private timer: any;
+
+  // Data do casamento
+  private dataCasamento = new Date('2026-11-20T16:00:00');
+
+  ngOnInit() {
+    this.atualizarContador();
+
+    this.timer = setInterval(() => {
+      this.atualizarContador();
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timer);
+  }
+
+  atualizarContador() {
+    const agora = new Date().getTime();
+    const destino = this.dataCasamento.getTime();
+
+    const diferenca = destino - agora;
+
+    if (diferenca <= 0) {
+      this.dias.set(0);
+      this.horas.set(0);
+      this.minutos.set(0);
+      this.segundos.set(0);
+      clearInterval(this.timer);
+      return;
+    }
+
+    this.dias.set(Math.floor(diferenca / (1000 * 60 * 60 * 24)));
+    this.horas.set(Math.floor((diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    this.minutos.set(Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60)));
+    this.segundos.set(Math.floor((diferenca % (1000 * 60)) / 1000));
+  }
+}
